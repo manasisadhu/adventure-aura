377e3bb8781ca3e69097a02bc153014b6946b7b7
manasisadhu <manasisadhu57@gmail.com>
2025-08-10 20:40:32 +0530

    UI bug fix

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,23 +5,23 @@ import { Button } from "../shadcnui/button";
 const Hero = () => {
 	return (
 		<>
-			<div className="gap-4 bg-[url('/home/hero.jpg')] bg-cover bg-center bg-no-repeat">
-				<div className="flex h-dvh flex-col items-start justify-center bg-black/40 bg-cover bg-center text-white">
-					<div className="mx-auto w-full max-w-7xl gap-6 px-6 py-10">
-						<Badge className="mb-6 rounded-4xl bg-white/30 p-2">
+			<div className="bg-[url('/home/hero.jpg')] bg-cover bg-center bg-no-repeat">
+				<div className="flex h-dvh flex-col justify-center bg-black/40 text-white">
+					<div className="mx-auto w-full max-w-7xl space-y-6 px-6 py-10">
+						<Badge className="rounded-4xl bg-white/30 p-2">
 							Experience Luxury Travel
 						</Badge>
 						<div className="text-6xl font-bold">
 							Explore the World <br />
 							Like Never Before
 						</div>
-						<div className="mt-5 text-2xl">
+						<div className="text-2xl">
 							Curated journeys to the worlds most extraordinary
 							destinations. Let us transform <br />
 							your travel dreams into unforgettable memories.
 						</div>
 
-						<div className="mt-6 flex flex-col gap-5 md:flex-row">
+						<div className="flex flex-col gap-5 md:flex-row">
 							<Button className="bg-orange-400 p-6 hover:bg-amber-500">
 								Start planning
 								<SquareArrowRight size={24} />
