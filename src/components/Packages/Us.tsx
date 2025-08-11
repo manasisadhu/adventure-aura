959600119e2b51e7d962596c8f23eb3528d22e97
manasisadhu <manasisadhu57@gmail.com>
2025-08-11 09:46:46 +0530

    review section done

--- src/components/Packages/Us.tsx
+++ src/components/Packages/Us.tsx
@@ -1,10 +1,17 @@
+import { Quote } from "lucide-react";
+
 const Us = () => {
 	return (
 		<>
-			<div className="mt-5 mb-5 h-[600] bg-[url('/home/home-tour.jpg')] bg-cover bg-center bg-no-repeat">
-				<div className="flex h-dvh flex-col items-center justify-center bg-black/30 bg-cover bg-center">
-					<div className="text-5xl font-bold text-white">
+			<div className="mt-5 h-[500] bg-[url('/home/home-tour.jpg')] bg-cover bg-center bg-no-repeat dark:bg-black">
+				<div className="flex h-dvh flex-col items-center justify-center">
+					<div className="flex text-5xl font-bold text-white">
+						<Quote
+							size={24}
+							className="scale-x-[-1]"
+						/>
 						Crafting Unforgetable Journeys
+						<Quote size={24} />
 					</div>
 				</div>
 			</div>
