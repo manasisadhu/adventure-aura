9f0a9fa3ebaa0e2293a129c8c57c210058ff5c47
manasisadhu <manasisadhu57@gmail.com>
2025-08-24 08:55:48 +0530

    Contact Form Validation Added !!

---
+++ src/lib/schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+export const contactSchema = z.object({
+	userName: z.string(),
+	userMail: z.email(),
+	mobileNo: z
+		.string()
+		.max(10, { message: "Please Enter a Valid Phone Number" })
+		.min(10, { message: "Please Enter a Valid Phone Number" }),
+	message: z
+		.string()
+		.min(10, { message: "Message must be at least 10 characters long." })
+		.max(500, { message: "Message cannot exceed 500 characters." })
+		.optional(),
+});
+
+export type contactSchemaType = z.infer<typeof contactSchema>;
