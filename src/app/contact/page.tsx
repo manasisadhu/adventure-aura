a7d2ec5518a14f63be55b80f93502ebbbe93604d
manasisadhu <manasisadhu57@gmail.com>
2025-08-25 10:46:08 +0530

    last updates done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,13 @@
 import ContactUi from "@/components/Contact/ContactUi";
 import CradsDisplay from "@/components/Contact/CradsDisplay";
 import ContactHeroUi from "@/components/Hero/ContactHeroUi";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "contact | Nextjs Starter Template",
+	description:
+		"We’d love to hear from you! Whether you have questions, suggestions, or just want to share your travel stories, Adventure Aura is here to connect. Reach out to us anytime—we’re happy to help and inspire your next journey.",
+};
 
 const page = () => {
 	return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,25 @@ import FooterTop from "@/components/Footer/FooterTop";
 import Header from "@/components/Header/Header";
 import ThemeProvider from "@/components/ThemeProvider";
 import { RootLayoutProps } from "@/lib/types";
+import { Metadata } from "next";
 import { Toaster } from "sonner";
 import "./globals.css";
+
+// Seo and Information
+export const metadata: Metadata = {
+	title: "Adventure Aura",
+	description:
+		"Adventure Aura is your gateway to unforgettable journeys, where exploration meets inspiration. From breathtaking destinations and hidden gems to travel tips and unique experiences, we bring you everything you need to fuel your wanderlust. Whether you’re seeking adrenaline-filled adventures, serene escapes, or cultural discoveries, Adventure Aura is here to guide and inspire every step of your journey.",
+	icons: {
+		icon: [
+			{
+				url: "/travel-bag.png",
+				type: "image/svg+xml",
+				sizes: "275x275",
+			},
+		],
+	},
+};
 
 const RootLayout = ({ children }: RootLayoutProps) => {
 	return (
